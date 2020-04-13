dce699dbc028e7897d919e9396facc7e75913915
David Obodo <obododavid5@gmail.com>
2020-04-13 19:06:57 +0100

    setting up environment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+let React = require('react');
 
-function App() {
+
+const Notification = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Notification
     </div>
-  );
+  )
+}
+
+
+const App = () => {
+
+  return (
+    <div id='app'>
+      <Notification />
+    </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+let ReactDomServer = require('react-dom/server'),
+    React = require('react'),
+    assert = require('chai').assert,
+    esc = require('escape-html');
+const toMarkup = ReactDomServer.renderToStaticMarkup;
+const toString = ReactDomServer.renderToString;
+const create = React.createElement;
+const App = require('./App')
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function withBootstrapStyles() {
+    // return {
+    //     alert_warning: 'red',
+    //     alert_success: 'green',
+    //     alert_info: 'black',
+    //     alert_danger: 'pink',
+    // }
+}
+
+describe('Notification', () => {
+    it('should render to some HTML', function () {
+        withBootstrapStyles();
+        let component = create(App, { notification: { message: 'Hello World!' } });
+        console.log('Rendering:<br/>' + toString(component));
+        console.log('Actual Markup:<br/>' + esc(toMarkup(component)));
+    })
+    it('should contain the message I pass in', function () {
+        let message = 'Notification to the end-user!'
+        let app = create(App, { notification: { message } });
+        assert.include(toMarkup(app), message)
+    })
 });
